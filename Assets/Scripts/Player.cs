920e6bb7787cbb09d6cd3f302d2856e8cd42d508
unknown <asir.zaki.v2@gmail.com>
2024-10-05 15:30:21 -0400

    player moves

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Vector3 direction;
+    public float moveSpeed = 1f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        direction = new Vector3(0, 0, 0);
+        if (Input.GetKey(KeyCode.A))
+            direction.x -= 1;
+        if (Input.GetKey(KeyCode.D))
+            direction.x += 1;
+        if (Input.GetKey(KeyCode.W))
+            direction.y += 1;
+        if (Input.GetKey(KeyCode.S))
+            direction.y -= 1;
+
+        transform.position += Time.deltaTime * moveSpeed * direction.normalized;
     }
 }
