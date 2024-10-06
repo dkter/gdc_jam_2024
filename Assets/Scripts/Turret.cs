d400863b4d3a6fde3159f4e5fb33abdfb0725ce9
David Teresi <dkteresi@gmail.com>
2024-10-06 15:02:17 -0400

    turrets shoot bullets

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -5,11 +5,23 @@ using UnityEngine;
 public class Turret : MonoBehaviour
 {
     public GameObject enemyParent;
+    public GameObject bulletPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(ShootTimer());
+    }
+
+    private IEnumerator ShootTimer()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(2);
+            GameObject newBullet = Instantiate(bulletPrefab);
+            newBullet.transform.position = this.transform.position + this.transform.rotation * new Vector3(0.5f, 0f, 0f);
+            newBullet.GetComponent<Bullet>().velocity = this.transform.rotation * Vector3.right * 4f;
+        }
     }
 
     // Update is called once per frame
